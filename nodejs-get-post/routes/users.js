87e718cb370b44587f327bc696fe206657ea7d06
Shubanshii <c_day@alumni.baylor.edu>
2017-11-16 00:57:42 -0700

    add get-post tut

---
+++ nodejs-get-post/routes/users.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.get('/detail', function(req, res, next) {
+  res.send('detail');
+});
+
+//localhost:8080/users/detail
+
+module.exports = router;
