e5f089b155a25367431375069b250f09ebc0c47e
Shubanshii <c_day@alumni.baylor.edu>
2017-11-16 01:14:56 -0700

    finished get post tut

--- nodejs-get-post/routes/index.js
+++ nodejs-get-post/routes/index.js
@@ -6,6 +6,15 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Cool, huh?!', condition: false, anyArray: [1,2,3] });
 });
 
+router.get('/test/:id', (req, res, next) => {
+	res.render('test', {output: req.params.id});
+});
+
+router.post('/test/submit', (req, res, next) => {
+	let id = req.body.id;
+	res.redirect('/test/' + id);
+});
+
 //could also get rid of users file
 //and add /users and /users/detail to this 
 //file
